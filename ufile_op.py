ad0b33c9e15613a3381ae97791d80428337a2874
delex.xie <delex.xie@ucloud.cn>
2019-05-07 17:37:28 +0800

    提交工具；支持mysql dump 备份到ufile

---
+++ ufile_op.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+import sys
+import os
+import json
+sys.path.append(os.path.realpath(os.path.dirname(__file__)))
+
+from ufile import filemanager
+from ufile import multipartuploadufile 
+from ufile.logger import logger, set_log_file
+#import argparse
+
+
+set_log_file()
+
+PUBLIC_KEY = ''
+PRIVATE_KEY = ''
+
+
+class UploadFailed(Exception):
+    pass
+
+
+class DownloadFailed(Exception):
+    pass
+
+
+
+#上传小文件，支持本地标准输入作为输入，参数local_file 填入 - 号
+def upload_put(bucket, key, local_file, header):
+    # 构造上传对象，并设置公私钥
+    handler = filemanager.FileManager(PUBLIC_KEY, PRIVATE_KEY)
+
+    if local_file == '-':
+        _, resp = handler.putstream(bucket, key, sys.stdin, header=header, mime_type='application/octec-stream')
+    else:
+        _, resp = handler.putfile(bucket, key, local_file, header=header)
+    if resp.status_code != 200:
+        print resp.error
+        raise UploadFailed()
+
+
+#上传大文件，支持本地标准输入作为输入，参数local_file 填入 - 号
+def upload_mput(bucket, key, local_file, header):
+    # 构造上传对象，并设置公私钥
+    handler = multipartuploadufile.MultipartUploadUFile(PUBLIC_KEY, PRIVATE_KEY)
+
+    if local_file == '-':
+        _, resp = handler.uploadstream(bucket, key, sys.stdin, header=header, mime_type='application/octec-stream')
+    else:
+        _, resp = handler.uploadfile(bucket, key, local_file, header=header)
+    if resp.status_code != 200:
+        print resp.error
+        raise UploadFailed()
+
+
+#下载文件
+def download(bucket, key, local_file, header):
+    # 构造下载对象，并设置公私钥
+    handler = filemanager.FileManager(PUBLIC_KEY, PRIVATE_KEY)
+
+    if local_file == '-':
+        local_file = '/dev/stdout'
+    _, resp = handler.download_file(bucket, key, local_file, header=header)
+    if resp.status_code != 200:
+        print resp.error
+        raise DownloadFailed()
+
+
+def usage():
+    print 'Usage:'
+    print '''
+{0} [upload_put|upload_mput|download] [domain] [key] [file]
+
+NOTE:
+    if file is "-", it means:
+    - stdin for upload
+    - stdout for download
+'''.format(__file__)
+
+
+def main():
+    if '-h' in sys.argv or '--help' in sys.argv or len(sys.argv)<4:
+        usage()
+        sys.exit(1)
+
+    global PUBLIC_KEY
+    global PRIVATE_KEY
+    
+    #读取配置文件
+    if os.path.exists('config.cfg'):		
+        f = open('config.cfg','r')
+        text = f.read()
+        f.close()
+        paras = json.loads(text)
+        PUBLIC_KEY = paras['public_key'].encode('unicode-escape').decode('string_escape')
+        PRIVATE_KEY = paras['private_key'].encode('unicode-escape').decode('string_escape')
+    else:
+        print "config.cfg not exists！"
+        sys.exit(1)
+		
+    if PUBLIC_KEY=="" or PRIVATE_KEY=="":
+        print "please set config.cfg keys！"
+        sys.exit(1)
+
+		#解析参数和处理
+    action = sys.argv[1]
+    domain = sys.argv[2]
+    key = sys.argv[3]
+    local_file = sys.argv[4]
+
+    if len(domain.split("."))<2:
+    	print 'domain error!'
+    	sys.exit(1)
+    bucket = domain.split(".")[0]
+
+    sys.stderr.write("{0} {1} {2} {3} {4}\n".format(os.path.realpath(__file__), action, domain, key, local_file ))
+    header = {
+        'Host':
+        '{0}'.format(domain)
+    }
+
+    if action == 'upload_put':
+        upload_put(bucket, key, local_file, header)
+    if action == 'upload_mput':
+        upload_mput(bucket, key, local_file, header)
+    elif action == 'download':
+        download(bucket, key, local_file, header)
+    else:
+	print "err cmd!"
+
+
+if __name__ == '__main__':
+    main()
