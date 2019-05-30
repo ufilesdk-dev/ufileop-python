d2fc7c3b1b50f4084e5f12c70cb6043fc68f86e3
delex.xie <delex.xie@ucloud.cn>
2019-05-30 15:10:53 +0800

    优化agent标识

--- ufile/config.py
+++ ufile/config.py
@@ -7,7 +7,7 @@ from . import __version__
 _sys_info = '{0}: {1}'.format(platform.system(), platform.machine())
 _python_ver = platform.python_version()
 
-USER_AGENT = 'UCloud Python SDK {0} ({1} : Python/{2})'.format(__version__, _sys_info, _python_ver)
+USER_AGENT = 'UCloud Python UfileOp {0} ({1} : Python/{2})'.format(__version__, _sys_info, _python_ver)
 UCLOUD_PROXY_SUFFIX = '.ufile.cn-north-04.ucloud.cn'
 UCLOUD_DOWNLOAD_SUFFIX = '.ufile.cn-north-04.ucloud.cn'
 UCLOUD_API_URL = 'http://api.ucloud.cn'
